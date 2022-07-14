51d417a1bbf9a623b088133d4d5e983081b3f169
lzyjin <1247y.j@gmail.com>
2022-07-14 15:09:58 +0900

    new portfolio

---
+++ portfolio/js/script.js
@@ -0,0 +1,32 @@
+(function() {
+    // mouse cursor effect
+    window.addEventListener('mousemove', (e) => {
+        const cursor = document.querySelector('.cursor');
+        const anchors = document.querySelectorAll('a');
+
+        window.addEventListener('scroll', () => {
+            gsap.to(cursor, {
+                duration: 0.4,
+                left: e.clientX + window.scrollX,
+                top: e.clientY + window.scrollY
+            });
+        });
+
+        gsap.to(cursor, {
+            duration: 0.4,
+            left: e.clientX + window.scrollX,
+            top: e.clientY + window.scrollY
+        });
+
+        anchors.forEach((v, i, arr) => {
+            v.addEventListener('mouseenter', () => {
+                cursor.classList.add('active');
+            });
+            v.addEventListener('mouseleave', () => {
+                cursor.classList.remove('active');
+            });
+        });
+    });
+
+
+})();
